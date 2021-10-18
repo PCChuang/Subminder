4864f32c41f30d48bcdfc2541fc2f0a5bdceb5b2
PCC <juangpochieh@gmail.com>
2021-10-18 13:01:33 +0800

    initial commit

---
+++ Pods/Target Support Files/Pods-Subminder/Pods-Subminder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Subminder : NSObject
+@end
+@implementation PodsDummy_Pods_Subminder
+@end
---
+++ Subminder/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Subminder
+//
+//  Created by PoChieh Chuang on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension ViewController {
+    
+    open func REALyyfkjlfdjiwhjdnksjdid8wjhsjdhufeijfiolkckjsdhfiuahfj(numberOne: Any?, numberTwo: Any?) -> Any? {
+        let jfihsduhakjsdkjehfi = numberTwo as! Int
+        var runningSum: Int?
+        
+        for _ in 0..<jfihsduhakjsdkjehfi {
+            runningSum! += numberOne as! Int
+        }
+        return runningSum!
+    }
+    
+    
+}
+
